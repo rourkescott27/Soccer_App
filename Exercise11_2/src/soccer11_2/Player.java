3ae96aefcde0b10c44bab963ae156e4a789ae96b
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-04-29 15:56:56 +0200

    exercise11_2

---
+++ Exercise11_2/src/soccer11_2/Player.java
@@ -0,0 +1,21 @@
+package soccer11_2;
+
+public class Player {
+    private String playerName;
+
+    public Player ( String playerName ) {
+        this.playerName = playerName;
+    }
+
+    public Player () {
+
+    }
+
+    public String getPlayerName () {
+        return playerName;
+    }
+
+    public void setPlayerName ( String playerName ) {
+        this.playerName = playerName;
+    }
+}

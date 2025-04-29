d46376204a6e89ff72bb9f10b4b0bf349ad7027d
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2025-04-29 15:59:14 +0200

    exercise13_2

---
+++ Exercise13_2/src/soccer13_2/Goal.java
@@ -0,0 +1,8 @@
+package soccer13_2;
+
+public class Goal extends GameEvent {
+
+    public String toString(){
+        return "Goal scored";
+    }
+}
---
+++ Exercise13_2/src/soccer13_2/Possession.java
@@ -0,0 +1,8 @@
+package soccer13_2;
+
+public class Possession extends GameEvent{
+
+    public String toString() {
+        return "Possession";
+    }
+}
